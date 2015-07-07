f0564c7a6e74f16a2adfdbe4896a0845ac8c4f21
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-07-07 09:35:57 +0100

    Add exercise 2.5 - compose

--- src/main/scala/com/czeczotka/scala/fpis/package02/GettingStarted.scala
+++ src/main/scala/com/czeczotka/scala/fpis/package02/GettingStarted.scala
@@ -11,6 +11,7 @@ object GettingStarted {
     example26_partial()
     exercise23_currying()
     exercise24_uncurry()
+    exercise25_composeTwoFunctions()
   }
 
   def example_241_factorial() {
@@ -51,9 +52,19 @@ object GettingStarted {
     val multiply = gs.uncurry[Int, Double, Double]((a: Int) => (b: Double) => a * b)
     println(s"uncurry: multiply($myInt, $myDouble) = ${multiply(myInt, myDouble)}")
   }
+
+  def exercise25_composeTwoFunctions() {
+    val myInt = 2
+    val add5Multiply2 = gs.compose[Int, Double, Double]((c: Double) => c * 2, (b: Int) => b + 5)
+    println(s"compose: add5Multiply2($myInt) = ${add5Multiply2(myInt)}")
+  }
 }
 
 class GettingStarted {
+
+  def compose[A,B,C](f: B => C, g: A=> B): A => C = {
+    (a: A) => f(g(a))
+  }
 
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
     (a: A, b: B) => f(a)(b)

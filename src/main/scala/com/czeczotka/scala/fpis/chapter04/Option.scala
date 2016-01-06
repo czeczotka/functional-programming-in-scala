eeb42e2e55c07c9a962b81b821b4b59bad1e2317
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2016-01-06 22:34:05 +0000

    Exercise 4.1 - 'flatMap' Option function

--- src/main/scala/com/czeczotka/scala/fpis/chapter04/Option.scala
+++ src/main/scala/com/czeczotka/scala/fpis/chapter04/Option.scala
@@ -7,7 +7,11 @@ sealed trait Option[+A] {
     case None => None
   }
 
-//  def flatMap[B](f: A => Option[B]): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(get) => f(get)
+    case None => None
+  }
+
 //  def getOrElse[B >: A](default: => B): B
 //  def orElse[B >: A](ob: => Option[B]): Option[B]
 //  def filter(f: A => Boolean): Option[A]
--- src/test/scala/com/czeczotka/scala/fpis/chapter04/OptionSpec.scala
+++ src/test/scala/com/czeczotka/scala/fpis/chapter04/OptionSpec.scala
@@ -5,11 +5,21 @@ import org.specs2.mutable.Specification
 class OptionSpec extends Specification {
 
   "Option" should {
-    "exercise 4.1: `map` method should apply `f` if the Option is not None" in {
+
+    "exercise 4.1: `map` function should apply `f` if the Option is not None" in {
       (None:Option[Int]).map((i: Int) => 0) shouldEqual None
       Some(2).map((i: Int) => i * i) shouldEqual Some(4)
       Some(-2).map((i: Int) => (i * i).toString) shouldEqual Some("4")
       Some("abc").map(_.length) shouldEqual Some(3)
+    }
+
+    "exercise 4.1: `flatMap` function should apply `f` if the Option is not None" in {
+      (None:Option[Int]).flatMap((i: Int) => Some(0)) shouldEqual None
+      Some(2).flatMap((i: Int) => Some(i * i)) shouldEqual Some(4)
+      Some(-2).flatMap((i: Int) => Some((i * i).toString)) shouldEqual Some("4")
+      Some("abc").flatMap((s: String) => Some(s.length)) shouldEqual Some(3)
+      Some(-2).flatMap((i: Int) => if (i >=  0) Some(Math.sqrt(i)) else None) shouldEqual None
+      Some(16).flatMap((i: Int) => if (i >=  0) Some(Math.sqrt(i)) else None) shouldEqual Some(4)
     }
   }
 }
